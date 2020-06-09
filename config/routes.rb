cdd48eaea15288586142512f9259617a26a673fd
vmoisiuk <moisiuk.volodymyr@gmail.com>
2020-06-09 12:41:33 +0300

    Sign Up Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
