05a9271d70285e025dd1004f777a635951931597
vmoisiuk <moisiuk.volodymyr@gmail.com>
2020-06-09 15:16:59 +0300

    Faker and seed file

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20200609115003) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
